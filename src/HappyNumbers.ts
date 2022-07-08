093f57f6fa7381fa7a06b125577188db70f67898
Weslley de Campos <weslley.campos@objective.com.br>
2022-07-08 14:58:12 -0300

    test: ensure HappyNumbers is done

--- src/HappyNumbers.ts
+++ src/HappyNumbers.ts
@@ -1,23 +1,23 @@
 export default class HappyNumbers {
 
-  public isHappynumberumber(number: number): boolean {
-    let map = {};
-  let tmp = 0;
+  public isHappyNumber(number: number): boolean {
+    const map = {};
+    let tmp = 0;
 
-  if (number < 1) return false;
+    if (number < 1) return false;
 
-  while (number !== 1 && !map[number]) {
-    map[number] = true;
-    tmp = 0;
+    while (number !== 1 && !map[number]) {
+      map[number] = true;
+      tmp = 0;
 
-    while (number > 0) {
-      tmp += Math.pow(number % 10, 2);
-      number = Math.floor(number / 10);
+      while (number > 0) {
+        tmp += Math.pow(number % 10, 2);
+        number = Math.floor(number / 10);
+      }
+
+      number = tmp;
     }
 
-    number = tmp;
-  }
-
-  return number === 1;
+    return number === 1;
   }
 }
--- test/HappyNumbers.test.ts
+++ test/HappyNumbers.test.ts
@@ -9,3 +9,17 @@ const makeSut = (): SutTypes => {
   
   return { sut }
 }
+
+describe('Happy Numbers', () => {
+  test('expect given number is happy', () => {
+    const { sut } = makeSut()
+
+    expect(sut.isHappyNumber(19)).toBeTruthy()
+  })
+
+  test('expect given number is not happy', () => {
+    const { sut } = makeSut()
+
+    expect(sut.isHappyNumber(8)).toBeFalsy()
+  })
+})
